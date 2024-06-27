94fe83066742c26a253ed1b7c531f9e20c119346
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-27 16:42:24 -0700

    CS V11

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ def main():
     """
     result = False
     
+    if email[0].isalpha():
+        
+        if 5 < len(email) < 30:
+            # Checks 'if' there is an '@' in email
+            if "@" in email:
+                at_index = email.find("@")
+            
+                if "." in email[at_index:]:
+                
+                    result = True
+                
+                print(result)
+                
+    
     
     
     
     """
     ########################################
     """
-    result = True
 
     ########################################
     # Do not delete the return statement
